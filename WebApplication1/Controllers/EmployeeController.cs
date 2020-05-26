164d3059c3bdc0591e654df4f633343807118805
ALLU SATYA SHANMUKHI <ALLU SATYA SHANMUKHI@DESKTOP-LPETQC4>
2020-05-26 13:02:22 +0530

    webapplication1

---
+++ WebApplication1/Controllers/EmployeeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/Employee")]
+    [ApiController]
+    public class EmployeeController : Controller
+    {
+        private IEmployeeRepository employeeRepository { get; set; }
+        public EmployeeController(IEmployeeRepository employeeRepository)
+        {
+            this.employeeRepository = employeeRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            List<Employee> employees = employeeRepository.GetEmployee();
+            return Ok(employees); ;
+        }
+    }
+}
+
---
+++ WebApplication1/Models/EmployeeRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class EmployeeRepository:IEmployeeRepository
+    {
+
+        public List<Employee> GetEmployee()
+        {
+            List<Employee> employees = new List<Employee>();
+
+            Employee employee = new Employee() { EmployeeId = 1, EmployeeName = "shanmukhi" };
+            employees.Add(employee);
+
+            employee = new Employee() { EmployeeId = 2, EmployeeName = "nikhila" };
+            employees.Add(employee);
+
+            return employees;
+        }
+    }
+}
